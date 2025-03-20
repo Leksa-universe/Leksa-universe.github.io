6ef9efc661b3cddb2a0cde4922148b04d6fe5e53
Leksa-universe <axel.barre28@gmail.com>
2025-03-20 13:25:21 +0100

    BubbleGame (first js)

---
+++ index.js
@@ -0,0 +1,37 @@
+// Injection variable (compteur)
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
+// Création de la function
+const bubbleGame = () => {
+  // Injection du span et de la classe
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  // Injection de tailles random
+  const size = Math.random() * 150 + 50 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  //  Injection de placement random
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  // Injection de déplacement random
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  // Explosion de bulle + injection compteurs de kills
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleGame, 800);
